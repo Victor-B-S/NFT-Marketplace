325b91f0f906d1aa23bf64a322ecfaeaa370af27
LeonardoSonco <leonardokarling@hotmail.com>
2023-07-11 18:12:43 -0300

    Finalizado(Falta o Header e Footer)

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -3,14 +3,13 @@ import RegisterForm from "./RegisterForm";
 import bgRegister from "../../images/Register/Image Placeholder.svg";
 
 import classes from "./Register.module.css";
+import ImageRC from "../UI/ImageRC";
 
 function Register() {
   
     return (
     <div className={classes.registerGrid}>
-      <div className={classes.registerBg}>
-      <img src={bgRegister}/>
-      </div>
+      <ImageRC img={bgRegister}/>
       <div className={classes.registerForm}>
         <h2>Create Account</h2>
         <p>
