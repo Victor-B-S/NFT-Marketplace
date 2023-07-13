39453596f2671cf2238cd6afce770e7c27420508
emanuellela <emanuellela@hotmail.com>
2023-07-13 15:54:58 -0300

    iniciando feature how it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import CardCateg from './components/UI/CardCateg';
 import BrowseCateg from './components/BrowseCateg';
+import CardHiw from './components/UI/CardHiw';
 
 function App() {
   return (
     <div className="app">
-      <CardCateg />
-      <BrowseCateg />
+{/*       <CardCateg />
+      <BrowseCateg /> */}
+      <CardHiw />
     </div>
   )
 }
---
+++ src/components/UI/CardHiw.jsx
@@ -0,0 +1,13 @@
+import classes from "./Cardhiw.module.css";
+
+function CardHiw(props) {
+  return (
+    <>
+        <div className={classes.CardHiw}>
+            <p>How It Works</p>
+        </div>
+    </>
+  );
+}
+
+export default CardHiw;
