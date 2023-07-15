f87188b20c672cf82c6cf7d4b27c75e53c5ed5a5
oesley <oesleyrm@gmail.com>
2023-07-15 15:28:06 -0300

    added component ArtistInfo

--- src/components/ArtistPage/ArtistPage.jsx
+++ src/components/ArtistPage/ArtistPage.jsx
@@ -1,12 +1,14 @@
 import Cards from "./Cards";
 import CoverProfile from "./CoverProfile";
+import ArtistInfo from "./ArtistInfo";
 import classes from "./ArtistPage.module.css";
 
 function ArtistPage() {
   return (
     <>
+      <CoverProfile />
+      <ArtistInfo />
       <div className={classes.container}>
-        <CoverProfile />
         <Cards />
       </div>
       <hr className={classes.hr}/>
--- src/components/ArtistPage/CoverProfile.jsx
+++ src/components/ArtistPage/CoverProfile.jsx
@@ -1,45 +1,23 @@
 import classes from "./CoverProfile.module.css";
 // import { imagePaths } from "./UI/imagePaths";
 import avatarImg from "../../images/ArtistPage/ArtistPage-avatar-placeholder.png";
-import { useEffect, useState } from "react";
 
 
 function CoverProfile() {
-
-  const [imgBg, setImgBg] = useState('/src/images/ArtistPage/ArtistPage-image-placeholder-1.png');
-
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 480) {
-        setImgBg('/src/images/ArtistPage/ArtistPage-image-placeholder-3.png');
-      }else if((window.innerWidth <= 768) && (window.innerWidth > 480)) {
-        setImgBg('/src/images/ArtistPage/ArtistPage-image-placeholder-2.png');
-      }else{
-        setImgBg('/src/images/ArtistPage/ArtistPage-image-placeholder-1.png');
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize(); 
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <>
-      <div className={classes.container}>
-        <img className={classes.imageCover} src={imgBg} alt="Image Background Placeholder"/>
-        <div className={classes.imageProfile}>
-          <div className={classes.avatar}>
-            <div className={classes.asset}>
-              <img className={classes.avatarImage} src={avatarImg} alt="Avatar Placeholder"/>
+      {/* <div className={classes.containerDesktop}> */}
+        <div className={classes.containerCover}>
+          <div className={classes.imageCover}></div>
+          <div className={classes.imageProfile}>
+            <div className={classes.avatar}>
+              <div className={classes.asset}>
+                <img className={classes.avatarImage} src={avatarImg} alt="Avatar Placeholder"/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </>
   );
 }
