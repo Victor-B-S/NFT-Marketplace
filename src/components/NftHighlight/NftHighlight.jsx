2d004d33ad458d63af4cede8d4d0856ee23666e9
Victor Bittencourt da Silva <victor-b.s@hotmail.com>
2023-07-16 14:15:41 -0300

    Pequeno update adicionando background

--- src/components/NftHighlight/NftHighlight.jsx
+++ src/components/NftHighlight/NftHighlight.jsx
@@ -5,12 +5,14 @@ import classes from "./NftHighlight.module.css"
 
 export default function NftHighlight() {
 
-    const targetDate = '2023-12-31T23:59:59';
+  const targetDate = '2023-12-31T23:59:59';
 
   return (
-    <div>
+    <div className={classes.container}>
+      <div className={classes.gradient}>
         <p>Auction Ends in:</p>
         <CountdownTimer targetDate={targetDate} />
+      </div>
     </div>
   )
 }
