3a198059cdcf941c6548f4b8f91ccbab621aa91c
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-07-17 09:56:17 -0300

    Ajustes
    - Ajustes de NFT Highlights pós merge

--- src/components/NftHighlight/NftHighlight.jsx
+++ src/components/NftHighlight/NftHighlight.jsx
@@ -5,22 +5,27 @@ import classes from "./NftHighlight.module.css"
 
 export default function NftHighlight() {
 
-  const targetDate = '2023-07-17T23:59:59';
-
+  const targetDate = '2023-07-21T23:59:59';
+ 
   return (
-    <div className={classes.container}>
+    <section className={classes.container}>
       <div className={classes.gradient}>
         <div className={classes.content}>
           <div className={classes.left}>
             <div className={classes.artistCard}><img src="../src/images/Avatar-7.png" />Shroomie</div>
-            <h2 className={classes.nftTitle}>Magic Mushrooms</h2>
-            <button className={classes.seeNFT}><img src="../src/images/Eye.svg" /><span>See NFT</span></button>
+            <h3 className={classes.nftTitle}>Magic Mushrooms</h3>
+            <div className={classes.web}>
+              <button className={classes.seeNFT}><img src="../src/images/Eye.svg" /><span>See NFT</span></button>
+            </div>
           </div>
           <div className={classes.right}>
             <CountdownTimer targetDate={targetDate} />
+            <div className={classes.mobile}>
+              <button className={classes.seeNFT}><img src="../src/images/Eye.svg" /><span>See NFT</span></button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar/navbar'
 import Footer from '../components/Footer/footer'
 import BrowseCateg from '../components/BrowseCategories/BrowseCateg'
 import HowWorks from '../components/HowItWorks/HowItWorks'
+import NFTHighlight from '../components/NftHighlight/NftHighlight'
 
 export default function () {
   return (
     <div>
       <Navbar />
       <BrowseCateg />
+      <NFTHighlight />
       <HowWorks />
       <Footer />
     </div>
