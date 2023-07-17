f5a2ff74fdd2547af7934967512f4ec146a2ce59
Victor Bittencourt da Silva <victor-b.s@hotmail.com>
2023-07-16 21:35:50 -0300

    artist card pronta

--- src/components/NftHighlight/NftHighlight.jsx
+++ src/components/NftHighlight/NftHighlight.jsx
@@ -5,12 +5,17 @@ import classes from "./NftHighlight.module.css"
 
 export default function NftHighlight() {
 
-  const targetDate = '2023-12-31T23:59:59';
+  const targetDate = '2023-07-17T23:59:59';
 
   return (
     <div className={classes.container}>
       <div className={classes.gradient}>
-        <CountdownTimer targetDate={targetDate} />
+        <div className={classes.content}>
+          <div className={classes.artistCard}><img src="../src/images/Avatar-7.png" />Shroomie</div>
+          <h2>Magic Mushrooms</h2>
+          <CountdownTimer targetDate={targetDate} />
+          <button>See NFT</button>
+        </div>
       </div>
     </div>
   )
